e4c744b098a931374d38dbee3a642ac51c7893b0
alexander <nesterov1441@gmail.com>
2023-10-07 21:21:16 +0300

    init

---
+++ src/main/java/ru/gaponov/Main.java
@@ -0,0 +1,7 @@
+package ru.gaponov;
+
+public class Main {
+    public static void main(String[] args) {
+        // в этом package пишем код. стараемся распределить по схожим директориям разные классы, чтобы не было каши
+    }
+}
---
+++ src/test/java/ru/gaponov/MainTest.java
@@ -0,0 +1,7 @@
+package ru.gaponov;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+}
